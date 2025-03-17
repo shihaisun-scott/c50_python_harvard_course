3fc76964a01696aa4873df184baad32ff9b02998
shihaisun-scott <shihaisun.scott@gmail.com>
2025-03-17 17:23:33 -0400

    learning dictionary

---
+++ 2_loops/hogwarts.py
@@ -0,0 +1,10 @@
+students = ["Hermione", "Harry", "Ron", "Draco"]
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+for student in students:
+    print(student)
+
+# indexing
+for i in range(len(students)):
+    print(i+1, students[i])
+
